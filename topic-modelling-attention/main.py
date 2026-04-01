03d3ba991cb80a775849fb4e2e163a9ad4342d55
Konstantin Nagaev <nagaev.kv@phystech.edu>
2026-04-01 17:34:18 +0300

    attentive_model

--- topic-modelling-attention/main.py
+++ topic-modelling-attention/main.py
@@ -7,33 +7,33 @@ from sklearn.datasets import fetch_20newsgroups
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-syspath.insert(1, r'D:\ARTM\topic-modelling-attention\src')
+syspath.insert(1, r'D:\ARTM\project_artm\topic-modelling-attention')
 
-import cartm.model2
+#from cartm.experimental_model import ExperimentalContextTopicModel
+from cartm.attentive_model import AttentiveTopicModel
 
-from cartm.experimental_model import ExperimentalContextTopicModel
 from cartm.preprocessing import DatasetPreprocessor
 from time import time
 
-#data = fetch_20newsgroups(data_home='./data/', subset='all').data
-with open('./data/test_data.txt') as f:
-    data = f.readlines()
+data = fetch_20newsgroups(data_home='./data/', subset='all').data
+#with open('./data/test_data.txt') as f:
+#    data = f.readlines()
 
 preprocessor = DatasetPreprocessor()
 tokenized_data, document_bounds = preprocessor.fit_transform(data)
 
-topic_model = ExperimentalContextTopicModel(
+attentive_topic_model = AttentiveTopicModel(
     vocab_size=len(preprocessor.vocabulary),
     ctx_len=10,
     n_topics=10
 )
 
 start_time = time()
-topic_model.fit(
+attentive_topic_model.fit(
     data=tokenized_data,
     ctx_bounds=document_bounds,
     verbose=2,
     seed=42,
 )
 end_time = time()
-print(f"{end_time - start_time}")
+print(f"attentive_topic_model {end_time - start_time}")
